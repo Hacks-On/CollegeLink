caf90d86a2654b9aff515b39490c90e9a69c8572
Kritik Modi <kritikmodi@gmail.com>
2021-09-16 01:14:21 +0530

    Adding files

---
+++ app/src/main/java/com/example/collegelink/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.collegelink;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    //private FirebaseAuth firebaseAuth;
+    //FirebaseUser firebaseUser;
+    String myuid;
+    ActionBar actionBar;
+    BottomNavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("Profile Activity");
+        //firebaseAuth = FirebaseAuth.getInstance();
+
+        navigationView = findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(selectedListener);
+        actionBar.setTitle("Home");
+
+        // When we open the application first
+        // time the fragment should be shown to the user
+        // in this case it is home fragment
+        HomeFragment fragment = new HomeFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content, fragment, "");
+        fragmentTransaction.commit();
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+
+                case R.id.nav_home:
+                    actionBar.setTitle("Home");
+                    HomeFragment fragment = new HomeFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content, fragment, "");
+                    fragmentTransaction.commit();
+                    return true;
+
+                case R.id.nav_profile:
+                    actionBar.setTitle("Profile");
+                    ProfileFragment fragment1 = new ProfileFragment();
+                    FragmentTransaction fragmentTransaction1 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction1.replace(R.id.content, fragment1);
+                    fragmentTransaction1.commit();
+                    return true;
+
+                case R.id.nav_notifications:
+                    actionBar.setTitle("Notifications");
+                    NotificationsFragment fragment2 = new NotificationsFragment();
+                    FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.content, fragment2, "");
+                    fragmentTransaction2.commit();
+                    return true;
+
+                case R.id.nav_chat:
+                    actionBar.setTitle("Chats");
+                    ChatboxFragment listFragment = new ChatboxFragment();
+                    FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction3.replace(R.id.content, listFragment, "");
+                    fragmentTransaction3.commit();
+                    return true;
+
+                case R.id.nav_addpost:
+                    actionBar.setTitle("Add Post");
+                    AddpostFragment fragment4 = new AddpostFragment();
+                    FragmentTransaction fragmentTransaction4 = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction4.replace(R.id.content, fragment4, "");
+                    fragmentTransaction4.commit();
+                    return true;
+            }
+            return false;
+        }
+    };
+}
